81623388bd1f8fbde592ece456081fec08c5cc9a
Joaquin Lopez <j04qin@gmail.com>
2026-04-13 14:33:35 -0600

    Implementación con js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const section = document.querySelector("main")
+
+
+let totalPosts = "";
+
 const posts = [
     {
         name: "Vincent van Gogh",
@@ -28,3 +33,63 @@ const posts = [
     }
 ]
 
+
+    for(let i = 0; i < posts.length; i++){
+    totalPosts += `
+                <section class="container-users">
+                
+                <div class="users">
+                    
+                    <div class="user-info">
+                        <img class="user-img" src="${posts[i].avatar}">
+                        <p class="bold">
+                            ${posts[i].name} <span class="location">${posts[i].location}</span>
+                        </p>
+                    </div>
+                    
+                    <div class="user-post">
+                        <img class="images" src="${posts[i].post}" alt="">
+                    </div>
+
+                    <div class="actions">
+                        <img class="icons heart" src="images/icon-heart.png" alt="">
+                        <img class="icons" src="images/icon-comment.png" alt="">
+                        <img class="icons" src="images/icon-dm.png" alt="">
+
+                            <div class="likes">
+                                <p class="bold">${posts[i].likes} likes</p>
+                            </div>
+                        
+                            <div class="comments">
+                                <p class="bold">${posts[i].username} <span class="ligth">${posts[i].comment}</span></p>
+                                
+                            </div>
+                    </div>
+
+                    <div class = "bottom"></div>
+
+                </div>
+                
+            </section>`
+ 
+    }
+
+  
+    section.innerHTML = totalPosts
+
+    const images = document.querySelectorAll(".images")
+    const heartIcon = document.querySelectorAll(".heart")
+
+    for (let i = 0; i < posts.length; i++){
+
+    function renderLikes () {
+        let newLikes = document.querySelectorAll(".likes .bold")[i];
+        posts[i].likes++;
+        console.log(newLikes);
+        newLikes.textContent = `${posts[i].likes} likes`
+    }
+
+    images[i].addEventListener ("dblclick", renderLikes)
+    heartIcon[i].addEventListener("click", renderLikes)
+    
+}
